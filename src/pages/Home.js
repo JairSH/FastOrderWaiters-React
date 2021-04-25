8cccc32c322a04d6d9894a477273f3b84927baba
jair <jair@hotmail.com>
2021-04-25 11:57:08 -0500

    modified Home design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../assets/styles/components/Home.css'
+import mainFondo from '../assets/statics/main-image.jpg'
 
 class Home extends React.Component {
   render () {
     return (
-      <h1>This is the Home!!!</h1>
+      <main>
+        <section className='main-image'>
+          <img src={mainFondo} alt='fondo' />
+        </section>
+        <section className='text-main'>
+          <p>Bienvenidos <br /> FAST FOOD</p>
+        </section>
+        <section className='main-buttons'>
+          <div>
+            <Link to='/login'>
+              <button>Login</button>
+            </Link>
+          </div>
+          <div>
+            <Link to='/register'>
+              <button>Register</button>
+            </Link>
+          </div>
+        </section>
+      </main>
     )
   }
 }
